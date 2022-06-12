884d51299e649e35db55d69b0716101560de902f
koodrg <56330018+koodrg@users.noreply.github.com>
2022-06-12 10:02:02 +0700

    api search ...

---
+++ src/controller/restaurant/getNearlyRestaurant.js
@@ -0,0 +1,18 @@
+const restaurant = require("../../models/restaurant");
+
+const getNearlyRestaurant = async (req, res) => {
+  try {
+    const restaurants = await restaurant.aggregate([{ $sample: { size: 10 } }])
+
+    if (restaurants) {
+      res.status(200).send({
+        total: restaurants.length,
+        data: restaurants,
+      });
+    }
+  } catch (error) {
+    res.status(404).send({ error: error.message });
+  }
+};
+
+module.exports = getNearlyRestaurant;
